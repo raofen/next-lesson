9bcb4829030f8dc29203066eedb5109bc534f541
raofen <trout.kazu@gmail.com>
2026-02-13 04:41:34 +0900

    6まで終わり

---
+++ app/news/page.tsx
@@ -0,0 +1,13 @@
+// news/page.tsx
+import { getNewsList } from "../_libs/microcms";
+import NewsList from "../_components/NewsList";
+
+export default async function Page() {
+  const { contents: news } = await getNewsList();
+
+  return (
+    <>
+      <NewsList news={news} />
+    </>
+  );
+}
