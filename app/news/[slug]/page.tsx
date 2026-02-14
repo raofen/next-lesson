6b7ed9403eadbb94f4965de4b6f54668e69a45f2
raofen <trout.kazu@gmail.com>
2026-02-14 14:38:47 +0900

    8、「ニュースにISRを適用」まで終わり

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -14,7 +14,7 @@ type Props = {
   };
 };
 
-export const revalidate = 0;
+export const revalidate = 60;
 
 export default async function Page({ params, searchParams }: Props) {
   const data = await getNewsDetail(params.slug, { draftKey: searchParams.dk }).catch(notFound);
