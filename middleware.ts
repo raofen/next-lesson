72a1cc88c7aacbfcfdfdf0ed5ef93703ed588618
raofen <trout.kazu@gmail.com>
2026-02-20 20:11:15 +0900

    8,Basic認証を適用

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+// middleware.ts
+
+import { NextRequest, NextResponse } from "next/server";
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
+
+export const middleware = createNextAuthMiddleware();
+export const config = {
+  matcher: ["/(.*)"],
+};
